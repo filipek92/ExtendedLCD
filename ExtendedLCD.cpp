55ddc8c1e63053bff8652062996e0dde11d15a7f
Filip Richter <richter.filip@gmail.com>
2016-08-12 14:15:34 +0200

    Added PSTR versions of functions

--- ExtendedLCD.cpp
+++ ExtendedLCD.cpp
@@ -31,12 +31,6 @@ ExtendedLCD::ExtendedLCD(uint8_t rs,  uint8_t enable,
 {
 }
 
-void ExtendedLCD::initText(const char line1[], const char line2[]){
-  strcpy(_line[0],line1);
-  strcpy(_line[1],line2);
-  if(!revert_time) revert();
-}
-
 void ExtendedLCD::revert(){
   clear();
   print(_line[0]);
@@ -54,30 +48,59 @@ void ExtendedLCD::doWork(){
 
 
 void ExtendedLCD::linePrint(const char line1[], const char line2[]){
+  strcpy(_line[0],line1);
+  strcpy(_line[1],line2);
   clear();
   print(line1);
   setCursor(0,1);
   print(line2);
-  strcpy(_line[0],line1);
-  strcpy(_line[1],line2);
+}
+
+void ExtendedLCD::linePrint_P(const char line1[], const char line2[]){
+  strcpy_P(_line[0],line1);
+  strcpy_P(_line[1],line2);
+  clear();
+  print(line1);
+  setCursor(0,1);
+  print(line2);
 }
 
 void ExtendedLCD::linePrint(const char value[], uint8_t line){
-  setCursor(0,line-1);
-  print(value);
   strcpy(_line[line-1],value);
+  setCursor(0,line-1);
+  print(_line[line-1]);
 }
 
+void ExtendedLCD::linePrint_P(const char value[], uint8_t line){
+  strcpy_P(_line[line-1],value);
+  setCursor(0,line-1);
+  print(_line[line-1]);
+}
 
 void ExtendedLCD::linePrint(const char value[]){
   linePrint(value, 1);
 }
 
-void ExtendedLCD::splash(const char line1[], const char* line2, unsigned long time){
+void ExtendedLCD::linePrint_P(const char value[]){
+  linePrint_P(value, 1);
+}
+
+void ExtendedLCD::splash(const char line1[], const char line2[], unsigned long time){
   clear();
   print(line1);
   setCursor(0,1);
   print(line2);
+  revert_time=millis()+time;
+}
+
+void ExtendedLCD::splash_P(const char line1[], const char line2[], unsigned long time){
+  char buffer[2][17];
+  strcpy_P(buffer[0], line1);
+  strcpy_P(buffer[1], line2);
+  clear();
+  print(buffer[0]);
+  setCursor(0,1);
+  print(buffer[1]);
   revert_time=millis()+time;
 }
 
@@ -87,13 +110,32 @@ void ExtendedLCD::splash(const char value[], uint8_t line, unsigned long time){
   revert_time=millis()+time;
 }
 
+void ExtendedLCD::splash_P(const char value[], uint8_t line, unsigned long time){
+  char buffer[17];
+  strcpy_P(buffer, value);
+  setCursor(0, line-1);
+  print(buffer);
+  revert_time=millis()+time;
+}
+
 void ExtendedLCD::setDisplay(const char line1[], const char line2[]){
   strcpy(_line[0],line1);
   strcpy(_line[1],line2);
   if(!revert_time) revert();
 }
 
+void ExtendedLCD::setDisplay_P(const char line1[], const char line2[]){
+  strcpy_P(_line[0],line1);
+  strcpy_P(_line[1],line2);
+  if(!revert_time) revert();
+}
+
 void ExtendedLCD::setDisplay(const char value[], uint8_t line){
   strcpy(_line[line-1], value);
+  if(!revert_time) revert();
+}
+
+void ExtendedLCD::setDisplay_P(const char value[], uint8_t line){
+  strcpy_P(_line[line-1], value);
   if(!revert_time) revert();
 }
--- ExtendedLCD.h
+++ ExtendedLCD.h
@@ -25,16 +25,22 @@ class ExtendedLCD: public LiquidCrystal
 		ExtendedLCD(uint8_t rs, uint8_t enable,
 			uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
 
-		void initText(const char line1[], const char line2[]);
 		void doWork();
 		void revert();
 		void linePrint(const char line1[], const char line2[]);
+		void linePrint_P(const char line1[], const char line2[]);
 		void linePrint(const char* value, uint8_t line);
+		void linePrint_P(const char* value, uint8_t line);
 		void linePrint(const char* l);
-		void splash(const char line1[], const char* line2, unsigned long time);
+		void linePrint_P(const char* l);
+		void splash(const char line1[], const char line2[], unsigned long time);
+		void splash_P(const char line1[], const char line2[], unsigned long time);
 		void splash(const char value[], uint8_t line, unsigned long time);
+		void splash_P(const char value[], uint8_t line, unsigned long time);
 		void setDisplay(const char line1[], const char line2[]);
+		void setDisplay_P(const char line1[], const char line2[]);
 		void setDisplay(const char value[], uint8_t line);
+		void setDisplay_P(const char value[], uint8_t line);
 	private:
 
 		unsigned long revert_time=0;
